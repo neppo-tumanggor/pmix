cc3c19ab9188dafdad13073264de30db9485c867
neppo-tumanggor <neppo@outlook.com>
2026-09-15 14:42:53 +0700

    feat: simplify pmix CLI usage without pnpm prefix

---
+++ pmix.ps1
@@ -0,0 +1,16 @@
+param(
+  [Parameter(ValueFromRemainingArguments = `$true)]
+  [string[]] `$args
+)
+
+`$scriptPath = Join-Path `$PSScriptRoot 'tools/scripts/pmix.cjs'
+if (-not (Test-Path `$scriptPath)) {
+  `$scriptPath = Join-Path `$PSScriptRoot 'tools\\scripts\\pmix.cjs'
+}
+
+if (-not (Test-Path `$scriptPath)) {
+  Write-Error "pmix script not found"
+  exit 1
+}
+
+& node `$scriptPath @args
